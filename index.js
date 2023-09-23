d998bbf0b38c0fe915f259bbd4c9138adaaac486
Dmitry Klemeshov <d.klemeshov@inmar.team>
2023-09-23 22:07:14 +0300

    fix header, fix stat border radius, add animation for window

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let Circle;
 let Cards;
+let Window;
 document.addEventListener("DOMContentLoaded", () => {
     Circle = document.getElementById('circle');
     Cards = document.getElementById('cards');
+    Window = document.getElementById('window');
 });
 const min = (a, b) => a > b ? b : a;
 
@@ -13,7 +15,10 @@ window.addEventListener('scroll', () => {
     if (Circle) {
         Circle.style.transform = `scale(${1 + min(posTop / 500, 1.5)})`;
     }
+    if (Window) {
+        Window.style.transform = `translate(0, -${min(posTop / 2, 400)}px)`;
+    }
     if (Cards) {
-        Cards.style.transform = `translate(0, ${min(posTop/5, 200)}px)`;
+        Cards.style.transform = `translate(0, ${min(posTop / 5, 200)}px)`;
     }
 })
