f4a74f2d3bc331ffa4e88c3e2aa5053e0f0fbaa4
Dmitry Klemeshov <d.klemeshov@inmar.team>
2023-09-23 21:20:56 +0300

    second page

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ window.addEventListener('scroll', () => {
     const posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
     if (Circle) {
-        Circle.style.transform = `scale(${1 + min(posTop / 500, 2)})`;
+        Circle.style.transform = `scale(${1 + min(posTop / 500, 1.5)})`;
     }
     if (Cards) {
         Cards.style.transform = `translate(0, ${min(posTop/5, 200)}px)`;
