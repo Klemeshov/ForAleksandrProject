340fc22eb6759ca451c6b1925a12e8314d482760
Dmitry Klemeshov <d.klemeshov@inmar.team>
2023-09-23 16:13:37 +0300

    add cards icons and stickers add cards animation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let Circle;
+let Cards;
 document.addEventListener("DOMContentLoaded", () => {
     Circle = document.getElementById('circle');
+    Cards = document.getElementById('cards');
 });
 const min = (a, b) => a > b ? b : a;
 
@@ -10,5 +12,8 @@ window.addEventListener('scroll', () => {
 
     if (Circle) {
         Circle.style.transform = `scale(${1 + min(posTop / 500, 2)})`;
+    }
+    if (Cards) {
+        Cards.style.transform = `translate(0, ${min(posTop/5, 200)}px)`;
     }
 })
