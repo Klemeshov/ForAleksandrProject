4ce31e5c2af0c3d6532294d388c4f67fd8d5f417
Dmitry Klemeshov <d.klemeshov@inmar.team>
2023-09-25 23:12:10 +0300

    video page

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ let Cards;
 let Window;
 let Icons;
 let LastWordBorder;
+let CardCreatedNumber;
+let NumberOfSales;
+let Income;
+
 document.addEventListener("DOMContentLoaded", () => {
     Circle = document.getElementById('circle');
     Cards = document.getElementById('cards');
     Icons = document.getElementById('icons');
     Window = document.getElementById('window');
     LastWordBorder = document.getElementById('lastWordBorder');
+    CardCreatedNumber = document.getElementById('cardCreatedNumber');
+    NumberOfSales = document.getElementById('numberOfSales');
+    Income = document.getElementById('income');
 
     setTimeout(() => {
-        if(LastWordBorder){
+        if (LastWordBorder) {
             LastWordBorder.style.transform = `rotate(-1.8deg)`;
         }
     }, 2000)
@@ -35,3 +42,31 @@ window.addEventListener('scroll', () => {
         Icons.style.transform = `translate(0, ${min(posTop / 4, 200)}px)`;
     }
 })
+
+let CardsCreatedValue = 50;
+let NumberOfSalesValue = 75;
+
+const onInputRangeChange = (element) => {
+    const percent = (element.value - element.min) / (element.max - element.min) * 100
+    element.style.background = `linear-gradient(90deg, rgba(238, 63, 80, 1) ${percent}%, rgba(138, 138, 138, 1) ${percent}%)`
+}
+
+onParametersChange = () => {
+    Income.innerText = `${CardsCreatedValue * NumberOfSalesValue * 70}.-`
+}
+
+const onCardsCreatedChange = (element) => {
+    onInputRangeChange(element);
+    CardsCreatedValue = element.value;
+    onParametersChange();
+    CardCreatedNumber.innerText = element.value;
+    CardCreatedNumber.style.left = `${3 + (element.value - 1) * 7.31}px`;
+}
+
+const onNumberOfSalesChange = (element) => {
+    onInputRangeChange(element);
+    NumberOfSalesValue = element.value;
+    onParametersChange();
+    NumberOfSales.innerText = element.value;
+    NumberOfSales.style.left = `${3 + (element.value - 1) * 4.857}px`;
+}
