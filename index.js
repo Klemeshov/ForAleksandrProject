5ae1a717a4ce492503b9e211e4d643a2ac7643ac
Dmitry Klemeshov <d.klemeshov@inmar.team>
2023-09-25 15:39:01 +0300

    Add animation for last word title, rework animations for cards and icons, add frame for background items

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let Circle;
 let Cards;
 let Window;
+let Icons;
+let LastWordBorder;
 document.addEventListener("DOMContentLoaded", () => {
     Circle = document.getElementById('circle');
     Cards = document.getElementById('cards');
+    Icons = document.getElementById('icons');
     Window = document.getElementById('window');
+    LastWordBorder = document.getElementById('lastWordBorder');
+
+    setTimeout(() => {
+        if(LastWordBorder){
+            LastWordBorder.style.transform = `rotate(-1.8deg)`;
+        }
+    }, 2000)
 });
 const min = (a, b) => a > b ? b : a;
 
@@ -16,9 +26,12 @@ window.addEventListener('scroll', () => {
         Circle.style.transform = `scale(${1 + min(posTop / 500, 1.5)})`;
     }
     if (Window) {
-        Window.style.transform = `translate(0, -${min(posTop / 2, 400)}px)`;
+        Window.style.transform = `translate(0, -${min(posTop / 3, 400)}px)`;
     }
     if (Cards) {
         Cards.style.transform = `translate(0, ${min(posTop / 5, 200)}px)`;
+    }
+    if (Icons) {
+        Icons.style.transform = `translate(0, ${min(posTop / 4, 200)}px)`;
     }
 })
